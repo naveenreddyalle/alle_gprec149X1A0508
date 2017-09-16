86dc1d1fbda0e41ba57ac40c6bae2be6137ec656
naveenreddyalle <31986481+naveenreddyalle@users.noreply.github.com>
2017-09-16 11:19:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+               Student s=new Student(508,"naveen reddy",1996-07-15,30);
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
